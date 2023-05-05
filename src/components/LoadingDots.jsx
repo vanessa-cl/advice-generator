c94432b72e1708edf83cd2d5534090c76bffab5e
vanessa-cl <vanessalima951@gmail.com>
2023-05-04 22:20:00 -0300

    feat: implementando componente de carregamento e ajustes no layout

---
+++ src/components/LoadingDots.jsx
@@ -0,0 +1,9 @@
+import "./styles/LoadingDots.css";
+
+export default function LoadingDots() {
+  return (
+    <div className="loading-area">
+      <div className="loading-dots"></div>
+    </div>
+  );
+}
--- src/components/Quote.jsx
+++ src/components/Quote.jsx
@@ -4,15 +4,22 @@ import { adviceSlipService } from "../services/AdviceSlipService";
 import "./styles/Quote.css";
 import pattern from "../assets/images/pattern-divider-desktop.svg";
 import SearchAdviceInput from "./SearchAdviceInput";
+import LoadingDots from "./LoadingDots";
+
+const INITIAL_SLIP_STATE = {
+  id: null,
+  advice: "",
+};
 
 export default function Quote() {
-  const [slip, setSlip] = useState({});
+  const [slip, setSlip] = useState(INITIAL_SLIP_STATE);
 
   useEffect(() => {
     getAdvice();
   }, []);
 
   const getAdvice = async () => {
+    setSlip(INITIAL_SLIP_STATE);
     return await adviceSlipService
       .getRandomAdvice()
       .then((response) => setSlip(response.slip))
@@ -25,7 +32,11 @@ export default function Quote() {
       <div className="quote-container">
         <h1 className="quote-title">ADVICE #{slip?.id}</h1>
         <div className="quote-area">
-          <p className="quote-text">&quot;{slip?.advice}&quot;</p>
+          {slip.advice ? (
+            <p className="quote-text">&quot;{slip?.advice}&quot;</p>
+          ) : (
+            <LoadingDots />
+          )}
         </div>
         <img className="pattern-divider" src={pattern} alt="pattern-divider" />
       </div>
